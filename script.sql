0279ca47d81fe3f013f97209b4e56ddfdfa2a2e3
Nghia <dmnghia1511@gmail.com>
2023-05-18 17:36:35 +0700

    Update script.sql

---
+++ script.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA `sellphonecard` ;
+
+USE sellphonecard;
+
+create table user (
+	id int not null AUTO_INCREMENT,
+	account varchar(255) not null,
+    password varchar(255) not null,
+    email varchar(255) not null,
+    role int,
+    phone_number varchar(255),
+    isDelete boolean,
+    primary key (id),
+    constraint user_unique unique key (email, account)
+);
