f9efbda87857e8d6f81f8b3a2b41022d301021ba
Nghia <dmnghia1511@gmail.com>
2023-05-21 19:46:54 +0700

    Update script file and update Function class

--- script.sql
+++ script.sql
@@ -3,13 +3,15 @@ CREATE SCHEMA `sellphonecard` ;
 USE sellphonecard;
 
 create table user (
-	id int not null AUTO_INCREMENT,
-	account varchar(255) not null,
-    password varchar(255) not null,
-    email varchar(255) not null,
-    role int,
-    phone_number varchar(255),
-    primary key (id),
-    constraint user_unique unique key (email, account)
+                      id int not null AUTO_INCREMENT,
+                      account varchar(255) not null,
+                      password varchar(255) not null,
+                      email varchar(255) not null,
+                      role int,
+                      phone_number varchar(255),
+                      isDelete boolean,
+                      primary key (id),
+                      constraint user_unique unique key (email, account)
 );
-
+alter table user rename column phone_number to phoneNumber;
+alter table user add column isActive boolean;
