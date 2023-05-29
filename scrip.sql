11aa09c7fdf256f3a29a4b4dd8f08aa05819e02b
Vu Van Manh (K16_HL) <manhvvhe161603@fpt.edu.vn>
2023-05-29 08:23:31 +0000

    Replace scrip.sql

--- scrip.sql
+++ scrip.sql
@@ -1,1 +1,32 @@
-scrip
+use sellPhoneCard;
+
+create table Product(
+	id int not null AUTO_INCREMENT,
+    name varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT null,
+    quantity int ,
+    price double,
+    supplier int,
+    createdAt datetime,
+    createdBy int ,
+    isDelete boolean,
+    deletedAt datetime,
+    deletedBy int,
+    updatedAt datetime,
+    updatedBy int,
+    primary key(id)
+);
+
+create table Supplier(
+	id int not null AUTO_INCREMENT,
+    name varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT null,
+    createdAt datetime,
+    createdBy int ,
+    isDelete boolean,
+    deletedAt datetime,
+    deletedBy int,
+    updatedAt datetime,
+    updatedBy int,
+    primary key(id)
+);
+
+alter table Supplier add image varchar(255);
